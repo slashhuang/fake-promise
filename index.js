b9424513e72dcbfe7d2054c6592afa4bc6fad669
slashhuang <627284482@qq.com>
2016-10-27 19:22:10 +0800

    update async call queue

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 /**
  * Created by slashhuang on 16/10/27.
- * write your own promise polyfill
+ * simple promise polyfill with then and catch funcionality
  */
 class Promise{
     constructor(asyncFn){
+        this.callQueue = []; //存储异步执行回调队列
 
+        asyncFn();
+        this.length=0;
+         alert('init');
     }
-    then(){
-
+    then(fn){
+        this.callQueue.push(fn);
+        return this
     }
     catch(){
+
+    }
+    compile(){
 
     }
 }
 
 let test1 = function(argument) {
-    // body...
+
 }
+new Promise(function(resolve,reject) {
+
+}).then(()=>{
+
+}).then(()=>{
+
+})
