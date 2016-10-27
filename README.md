8db77224998c33e0105c7cac9d58d31b7dcbdf2f
slashhuang <627284482@qq.com>
2016-10-27 19:12:02 +0800

    update to ismorphic

--- README.md
+++ README.md
@@ -1,37 +1,6 @@
-# write-your-own-mvvm【100行代码实现一个极简的mvvm实现方案】
+# ismorphic-promise
 
-# 安装和运行
-
-```bash
-    npm install && npm start
-    open localhost:8081/webpack-dev-server/
-```
-
-### [主程序入口 index.js](https://github.com/slashhuang/write-your-own-mvvm/blob/master/index.js)
-
-### 实现的功能点
-
-- 模板引擎指令解析
-- setter 和 getter指令
-- 虚拟dom技术
-- 仅支持单个dom节点
-
-### 未实现的功能点
-
-- dom遍历生成虚拟dom
-- diff算法优化
-- 扩展性不强
-- 事件代理
-
-### 特点
-
-> 基本展示了MVVM框架实现的基本技术要点
-
-> 可以作为学习vue/ng等千行级别的框架源码入门指南
-
-### Licence  
-The MIT License (MIT)
-Copyright (C) 2016 ~ [slashhuang](http://github.com/slashhuang) 627284482@qq.com
+> 同构JS promise异步处理模块
 
 
 
--- index.js
+++ index.js
@@ -1,103 +1,19 @@
 /**
- * Created by slashhuang on 16/10/26.
+ * Created by slashhuang on 16/10/27.
+ * write your own promise polyfill
  */
-import $ from 'jquery';
-const reg =/@(.*)/i; //事件正则
-const musReg = /{{(.*)}}/i; //模板绑定
-class MVVM {
-    constructor(config,target){
-        this.target = target;
-        this.config =config;
-        this.virtualDom={};
-        //主逻辑入口
-        this.logic()
+class Promise{
+    constructor(asyncFn){
+
     }
-    logic(){
-        this.defineGetterSetter();
-        this.parser();//解析模板,生成虚拟dom
-        this.addEvent(this.virtualDom); //挂载事件
-        this.render();//渲染到dom节点
+    then(){
+
     }
-    defineGetterSetter(){
-        let {data} = this.config;
-        let $data = data();
-        //定义简化版的getter setter
-        let self =this;
-        for(let key in $data){ //get set仍旧挂载在this.config上面
-            Object.defineProperty(this.config, key, {
-                set: function(newVal) {
-                    self.digest(key,newVal);
-                    return newVal
-                }
-            });
-        }
+    catch(){
+
     }
-    //更新dom
-    digest(key,val){
-        this.virtualDom.data[key] = val;
-        this.render();
-    }
-    parser() {
-        let { template,data} =this.config;
-        //初始化的数据
-        let $data =data();
-        //存储dom节点对应的指令
-        let cmdList=[];
-        //生成dom节点 createDocumentFragment
-        let fragment = $(template);
-        //解析dom节点的属性
-        let $attributes = [].slice.call(fragment[0].attributes);
-        $attributes && $attributes.forEach((attr)=>{
-            //存储指令列表
-            cmdList.push({
-                event:reg.exec(attr.name)[1],
-                callbackName:attr.value
-            });
-        });
-        //解析dom节点的innerHTML对应的key值
-        let keyName=musReg.exec(fragment.html())[1];
-        //生成虚拟dom结构
-        let fragVdom= {
-            fragment:fragment, //dom
-            cmdList:cmdList,  //指令
-            data:$data,//数据
-            watcherKey:keyName,//依赖的键值数组
-            renderFunc:function(){ //渲染方式
-                return this.fragment.html(this.data[this.watcherKey]);
-            },
-            children:[]
-        };
-        //这里只是个简单演示,并未做遍历dom结构处理
-        this.virtualDom = fragVdom;
-    }
-    addEvent(vDom){
-        let {fragment,cmdList} = vDom;
-        cmdList && cmdList.forEach((cmd)=>{
-            let {
-                event,callbackName
-            } = cmd;
-            fragment.on(event,this.getMethod(callbackName))
-        });
-    }
-    getMethod(name){
-        let { methods }= this.config;
-        return ()=>methods[name].apply(this.config); //保证引用中的this指向config文件
-    }
-    render() {
-        this.target.empty().append(this.virtualDom.renderFunc());
-    }
-};
-let config = {
-    template : '<div @click="changeTest">{{test}}</div>',
-    data(){
-        return {
-            test:'您初始化了所有的dom节点'
-        }
-    },
-    methods:{
-        changeTest:function(e){
-            this.test = '点击了'
-        }
-    }
-};
-setTimeout(()=>new MVVM(config,$('#app')),0);
+}
+
+let test1 = function(argument) {
+    // body...
+}
