d083ab830ed006518b622b82e76139fba68927d1
slashhuang <627284482@qq.com>
2016-10-30 23:05:31 +0800

    finish all Promise functions

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ class Promise{
                         let $fn = _1stFn[keyName];
                         // 执行函数
                         if( typeof $fn == 'function'){
-                            console.warn(`执行函数为链式调用的第${C_length-this.callQueue.length}个`)
+                            // console.warn(`执行函数为链式调用的第${C_length-this.callQueue.length}个`)
                             this.chaindHandler($fn(val));
                         }else{
                             //继续寻找下一个能够处理的函数
@@ -97,7 +97,7 @@ class Promise{
             return this;
         }
     }
-    /*根据then和catch处理逻辑*/
+    /*根据then和catch处理逻辑,同步则立即处理、异步则缓存在this.callQueue中*/
     handlelPromise(type){
         return (resFn,rejFn)=>{
             //过滤处理函数
@@ -138,32 +138,48 @@ class Promise{
        return this.handlelPromise('catch')(rejFn);
     }
     /* 静态方法 处理Promise的逻辑 */
-    static race(...proimiseInstance){
+    static race(PromiseInstance){
        //直接借用Promise的then方法，捕获数据,只要触发一次，Promise就seal住不接受后续处理，因此还是比较简单
         return new Promise((res,rej)=>{
-            proimiseInstance.forEach(($Promise)=>{
-                    $Promise.then(
+            PromiseInstance.forEach(function($Promise){
+                //如果是Promise才需要处理
+                if($Promise instanceof Promise){
+                     $Promise.then(
                         val=>res(val),
                         val=>rej(val)
                     );
-                });
+                 }else{
+                    res($Promise);
+                 }
+             });
             });
     }
-    static all(...proimiseInstance){
+    static all(PromiseInstance){
          //直接借用Promise的then方法，捕获数据
-         let $status = [];
-        let triggerFn=()=>{
-            //都返回结果后
-            if($status.length  = proimiseInstance.length){
+        let $statusValue = [];
+        let triggerFn=(res,rej)=>(status,value)=>{
+            //如果有一个rejected，则认为已经出结果了
 
+            if(status =='rejected'){
+                rej(value);
+                return;
+            };
+            $statusValue .push(value);
+            //都返回结果后
+            if($statusValue.length  == PromiseInstance.length){
+                res($statusValue)
             }
         } 
         return new Promise((res,rej)=>{
-            proimiseInstance.forEach(($Promise)=>{
+            PromiseInstance.forEach(($Promise)=>{
+                 if($Promise instanceof Promise){
                      $Promise.then(
-                        val=>$status.push({status:'resolved',val}),
-                        val=>$status.push({status:'rejected',val})
+                        val=>triggerFn(res,rej)('resolved',val),
+                        val=>triggerFn(res,rej)('rejected',val)
                     );
+                 }else{
+                    triggerFn(res,rej)('',$Promise);
+                 }
                 });
             });
     }
@@ -191,30 +207,135 @@ class Promise{
 // });
 
 /* 测试2用来 测试异步情况下，队列形式的函数处理 ======测试通过======*/
-let test2  = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        console.log('async  ==>  level 1');
-        resolve('async level 1')
-    },100)
+// let test2  = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log('async  ==>  level 1');
+//         resolve('async level 1')
+//     },100)
+// })
+// .then('1') //参数无法处理Promise状态，寻找下一个执行函数
+// .then((val)=>{
+//         console.warn(val,'---- executed');
+//         return new Promise((res,rej)=>{
+//             console.log('sync after async ==> level 2');
+//             res('sync after async level 2')
+//         })
+// })
+// .then((val)=>{
+//         console.warn(val,'---- executed');
+//         return new Promise((res,rej)=>{
+//             console.log('sync after async ==> level 2');
+//             throw new TypeError('sync error thrown');
+//         })
+// })
+// .catch((val)=>{
+//     console.log('catch=====',val)
+// });
+
+/* 测试3用来 测试静态方法 race + all ======测试通过======*/
+
+// let t1 = ()=>new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.error('测试静态方法===Promise.all')
+//         resolve('Promise.all 1')
+//     },100)
+// })
+
+// console.error('测试静态方法===Promise.race')
+// let t2 =()=> new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.error('测试静态方法===Promise.all')
+//           resolve('Promise.all 2')
+//     },110)
+// });
+// Promise.all(t1(),t2()).then((val)=>console.dir(JSON.stringify(val)));
+
+// Promise.race(t1(),t2()).then((val)=>console.dir(JSON.stringify(val)));
+
+
+
+new Promise((res,rej)=>{
+  res(1)
 })
-.then('1') //参数无法处理Promise状态，寻找下一个执行函数
-.then((val)=>{
-        console.warn(val,'---- executed');
-        return new Promise((res,rej)=>{
-            console.log('sync after async ==> level 2');
-            res('sync after async level 2')
-        })
-})
-.then((val)=>{
-        console.warn(val,'---- executed');
-        return new Promise((res,rej)=>{
-            console.log('sync after async ==> level 2');
-            throw new TypeError('sync error thrown');
-        })
-})
-.catch((val)=>{
-    console.log('catch=====',val)
+.then((val)=>{console.log(val);return new Promise(()=>{
+  throw new TypeError('fuck')
+})},(val)=>console.log('rejected then1===',val))
+.then((val)=>console.log(val),(val)=>console.log('rejected===',val))
+.catch((val)=>console.log('rejected=catch==',val))
+
+// -------------------静态方法-------------------
+/* Promise.all 等待回调全部返回 */
+var t1 = 1;
+var t2 = 1337;
+var t3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
 });
+
+Promise.all([t1, t2, t3]).then(values => {
+  console.log(values); // [3, 1337, "foo"]
+});
+var p1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "one");
+});
+var p2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, "two");
+});
+var p3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, "three");
+});
+var p4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 4000, "four");
+});
+var p5 = new Promise((resolve, reject) => {
+  reject("reject");
+});
+
+Promise.all([p1, p2, p3, p4,p5]).then(values => {
+  console.log(values);
+}).catch(reason => {
+  console.log(reason)
+});
+
+/** The Promise.race(iterable) 取回调函数返回的第一个**/
+// --------------例1
+var p1 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 500, "one");
+});
+var p2 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, "two");
+});
+Promise.race([p1, p2]).then(function(value) {
+  console.log(value); // "two"
+  // Both resolve, but p2 is faster
+});
+// --------------例2
+var p3 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, "three");
+});
+var p4 = new Promise(function(resolve, reject) {
+    setTimeout(reject, 500, "four");
+});
+
+Promise.race([p3, p4]).then(function(value) {
+  console.log(value); // "three"
+  // p3 is faster, so it resolves
+}, function(reason) {
+  // Not called
+});
+// // --------------例3
+var p5 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 500, "five");
+});
+var p6 = new Promise(function(resolve, reject) {
+    setTimeout(reject, 100, "six");
+});
+Promise.race([p5, p6]).then(function(value) {
+  // Not called
+}, function(reason) {
+  console.log(reason); // "six"
+  // p6 is faster, so it rejects
+});
+
 
 
 
